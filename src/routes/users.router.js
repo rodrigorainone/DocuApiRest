56df77b1dab93a658a0098aea6f7f0268fa1303c
Tuky <rodri_cjs22@hotmail.com>
2023-08-20 18:48:20 -0300

    docu api rest

---
+++ src/routes/users.router.js
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+import { privacy , handlePolicies} from "../middlewares/auth.js";
+import userController from "../controllers/users.controller.js"
+
+const router = Router();
+
+
+
+router.put('/premium/:uid',privacy('PRIVATED'),handlePolicies(['USER',"PREMIUM","ADMIN"]),userController.changeRole)
+router.get('/',userController.getUserAllDTO)
+router.delete('/:uid',userController.deleteUser)
+
+
+export default router;
---
+++ src/services/Repositories/usersRepository.js
@@ -0,0 +1,27 @@
+export default class UsersRepository {
+    constructor(dao){
+        this.dao =dao;
+    }
+    createUser = (user)=>{
+        return this.dao.createUser(user);
+    }
+    getUser= (user)=>{
+        return this.dao.getUser(user);
+    }
+
+    getUserAll = () =>{
+        return this.dao.getUserAll()
+     }
+
+    updateUser = (id, user) => {
+        return this.dao.updateUser(id,user);
+      };
+    
+      deleteUser = (id) => {
+        return this.dao.deleteUser(id);
+      };
+
+    changeRole = (user)=>{
+        return this.dao.changeRole(user);
+    }
+}
